f4b9c608172e230dc27e77ea46bedf84332bf936
Rebekah <rebekahjanedejong@gmail.com>
2021-10-15 06:42:36 -0400

    Use data from API to populate MealHeader component

--- src/Components/MealHeader/MealHeader.js
+++ src/Components/MealHeader/MealHeader.js
@@ -3,12 +3,12 @@ import Title from '../Title/Title';
 import Subtitle from '../Subtitle/Subtitle';
 import './MealHeader.css';
 
-function MealHeader() {
+function MealHeader({ title, time, servings }) {
     return (
         <div className="meal-header">
-            <Title title="Soba Noodle Salad" />
-            <Subtitle text="Total Time: 15min" />
-            <Subtitle text="Servings: 4" />
+            <Title title={title} />
+            <Subtitle text={`Ready in ${time} min`} />
+            <Subtitle text={`Servings: ${servings}`} />
         </div>
     );
 };

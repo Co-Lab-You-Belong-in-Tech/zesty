c6f4b9d69bfdc5660a9225f0cc43c679c045b3a5
blakebdavies <81705490+blakebdavies@users.noreply.github.com>
2021-10-27 23:02:03 +0100

    updated help page styling

--- src/Pages/Help.js
+++ src/Pages/Help.js
@@ -1,31 +1,73 @@
 import React from "react";
 import "../App.css";
-import ZestyLogo from "../logo.png";
+import RadsImg from "../Rads-01.png";
+import CheckIcon from "@mui/icons-material/Check";
+import {
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+  Box,
+  makeStyles,
+} from "@material-ui/core";
+
+import "../App.css";
+
+const useStyles = makeStyles(() => ({
+  icon: {
+    color: "blue",
+  },
+}));
 
 function Help() {
   return (
-    <div className="home">
-      <div className="container">
-        <h1 className="help-title">Your Meal Planning Assistant Made EASY!</h1>
-        <ul className="helpful-tips">
-          <li className="helpful-tip-item">
-            Set your meal PREFERENCES based on serving size, allergies, and
-            more!
-          </li>
-          <li className="helpful-tip-item">
-            Easily SEARCH for your favorite meals!
-          </li>
-          <li className="helpful-tip-item">
-            SAVE your meals and a shopping list is automatically created for
-            you!
-          </li>
-          <li className="helpful-tip-item">
-            Don't lose what you saved! EMAIL multiple meals to yourself before
-            you close the app.
-          </li>
-        </ul>
-        <img src={ZestyLogo} alt="logo" className="logo" />
-      </div>
+    <div className="help">
+      <br />
+      <List>
+        <ListItem>
+          <ListItemText>
+            <Box display="flex" alignItems="center">
+              <CheckIcon />{" "}
+              <Typography>
+                Set your meal PREFERENCES based on serving size, allergies, and
+                more!
+              </Typography>
+            </Box>
+          </ListItemText>
+        </ListItem>
+        <ListItem>
+          <ListItemText>
+            <Box display="flex" alignItems="center">
+              <CheckIcon />{" "}
+              <Typography>Easily SEARCH for your favorite meals!</Typography>
+            </Box>
+          </ListItemText>
+        </ListItem>
+        <ListItem>
+          <ListItemText>
+            <Box display="flex" alignItems="center">
+              <CheckIcon />{" "}
+              <Typography>
+                SAVE your meals and a shopping list is automatically created for
+                you!
+              </Typography>
+            </Box>
+          </ListItemText>
+        </ListItem>
+        <ListItem>
+          <ListItemText>
+            <Box display="flex" alignItems="center">
+              <CheckIcon />{" "}
+              <Typography>
+                Don't lose what you saved! EMAIL multiple meals to yourself
+                before you close the app.
+              </Typography>
+            </Box>
+          </ListItemText>
+        </ListItem>
+      </List>
+
+      <img src={RadsImg} alt="radishes" className="rads-img" />
     </div>
   );
 }
--- src/Pages/MealList.js
+++ src/Pages/MealList.js
@@ -1,13 +1,13 @@
 import React, { useContext } from "react";
 import { FavoriteContext } from "../Contexts/FavoriteContext";
-import Box from "@mui/material/Box";
+// import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import PropTypes from "prop-types";
+// import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
 const theme = createTheme({
@@ -15,27 +15,6 @@ const theme = createTheme({
     background: "#6a784d;",
   },
 });
-
-function Item(props) {
-  const { sx, ...other } = props;
-  return (
-    <Box
-      sx={{
-        p: 1,
-        borderRadius: 1,
-        textAlign: "center",
-        fontSize: "1rem",
-        fontWeight: "400",
-        ...sx,
-      }}
-      {...other}
-    />
-  );
-}
-
-Item.propTypes = {
-  sx: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
-};
 
 function MealList(meal) {
   const { favorites } = useContext(FavoriteContext);
@@ -49,40 +28,28 @@ function MealList(meal) {
     // };
     return (
       <ThemeProvider theme={theme}>
-        <Box
-          sx={{
-            display: "grid",
-            columnGap: 3,
-            rowGap: 1,
-            gridAutoFlow: "row",
-            gridTemplateColumns: "repeat(2, 1fr)",
-          }}
-        >
-          <Item>
-            <CardActions>
-              <Link key={favorite.id} to={`/meal/${favorite.id}`}>
-                <Card sx={{ maxWidth: 600 }}>
-                  <CardMedia
-                    component="img"
-                    height="140"
-                    image={favorite.image}
-                    title={favorite.title}
-                  />
-                  <CardContent className="recipe-card">
-                    <Typography
-                      gutterBottom
-                      // variant="h6"
-                      component="div"
-                      className="recipe-title"
-                    >
-                      {favorite.title}
-                    </Typography>
-                  </CardContent>
-                </Card>
-              </Link>
-            </CardActions>
-          </Item>
-        </Box>
+        <CardActions>
+          <Link key={favorite.id} to={`/meal/${favorite.id}`}>
+            <Card sx={{ maxWidth: 600 }}>
+              <CardMedia
+                component="img"
+                height="140"
+                image={favorite.image}
+                title={favorite.title}
+              />
+              <CardContent className="recipe-card">
+                <Typography
+                  gutterBottom
+                  // variant="h6"
+                  component="div"
+                  className="recipe-title"
+                >
+                  {favorite.title}
+                </Typography>
+              </CardContent>
+            </Card>
+          </Link>
+        </CardActions>
       </ThemeProvider>
     );
   });

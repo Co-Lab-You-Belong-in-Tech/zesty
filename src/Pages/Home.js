a994e04734a190b39c190fc852594185f8b828bb
Rebekah <rebekahjanedejong@gmail.com>
2021-10-28 10:55:47 -0400

    Add message for newcomer

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import PageTitle from '../Components/PageTitle/PageTitle';
 import SearchBar from "../Components/SearchBar/SearchBar";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
     <div className="home">
       <PageTitle text="Hello! What are you planning to make?" />
+      <div>New to Zesy? Click <Link to="/help"><span className="help-link">here</span></Link> to learn about the app!</div>
       <SearchBar />
     </div>
   );

613f78154abf4c3ce343487c356c875a03997a7d
Rebekah <rebekahjanedejong@gmail.com>
2021-11-01 21:51:54 -0400

    Fix alignment on Help page

--- src/Pages/Help.js
+++ src/Pages/Help.js
@@ -4,90 +4,42 @@ import "../App.css";
 import RadsImg from "../Rads-01.png";
 import CheckIcon from "@mui/icons-material/Check";
 import FeedbackIcon from "@mui/icons-material/Feedback";
-import {
-  List,
-  ListItem,
-  ListItemText,
-  Typography,
-  Box,
-} from "@material-ui/core";
+
 
 import "../App.css";
 
 function Help() {
   return (
-    <div className="help">
+    <section className="help">
       <PageTitle
-        text="Your Meal Planning Assistant Made EASY!"
+        text="Meal Planning Made EASY!"
         className="help-title"
       />
       <hr />
-      <List>
-        <ListItem>
-          <ListItemText>
-            <Box display="flex" alignItems="center">
-              <CheckIcon className="helpScreen-icon" />{" "}
-              <Typography>
-                Set your meal PREFERENCES based on serving size, allergies, and
-                more!
-              </Typography>
-            </Box>
-          </ListItemText>
-        </ListItem>
-        <ListItem>
-          <ListItemText>
-            <Box display="flex" alignItems="center">
-              <CheckIcon className="helpScreen-icon" />{" "}
-              <Typography>Easily SEARCH for your favorite meals!</Typography>
-            </Box>
-          </ListItemText>
-        </ListItem>
-        <ListItem>
-          <ListItemText>
-            <Box display="flex" alignItems="center">
-              <CheckIcon className="helpScreen-icon" />{" "}
-              <Typography>
-                SAVE your meals and a shopping list is automatically created for
-                you!
-              </Typography>
-            </Box>
-          </ListItemText>
-        </ListItem>
-        <ListItem>
-          <ListItemText>
-            <Box display="flex" alignItems="center">
-              <CheckIcon className="helpScreen-icon" />{" "}
-              <Typography>
-                Don't lose what you saved! EMAIL multiple meals to yourself
-                before you close the app.
-              </Typography>
-            </Box>
-          </ListItemText>
-        </ListItem>
-        <div className="feedback-message">
-          <ListItem>
-            <ListItemText>
-              <Box display="flex" alignItems="center">
-                <FeedbackIcon className="helpScreen-icon" />
-                <Typography>
-                  As we continue to build Zesty, we'd love your feedback! Feel
-                  free to send us an
-                  <a
-                    href="mailto:zestier.than.ever@gmail.com"
-                    className="help-link"
-                  >
-                    {" "}
-                    email
-                  </a>{" "}
-                  with your ideas!
-                </Typography>
-              </Box>
-            </ListItemText>
-          </ListItem>
-        </div>
-      </List>
+      <article className="help-item">
+        <CheckIcon className="helpScreen-icon" />
+        <p>Set your meal PREFERENCES based on serving size, allergies, and more!</p>
+      </article>
+      <article className="help-item">
+        <CheckIcon className="helpScreen-icon" />
+        <p>Easily SEARCH for your favorite meals!</p>
+      </article>
+      <article className="help-item">
+        <CheckIcon className="helpScreen-icon" />
+        <p>Coming Soon! SAVE your meals and a shopping list is automatically created for you!</p>
+      </article>
+      <article className="help-item">
+        <CheckIcon className="helpScreen-icon" />
+        <p>Don't lose what you saved! EMAIL recipes to yourself before you close the app.</p>
+      </article>
+      <article className="help-item">
+        <FeedbackIcon className="helpScreen-icon" />
+        <p>As we continue to build Zesty, we'd love your feedback! Feel free to fill out our 
+          <a href="https://docs.google.com/forms/d/e/1FAIpQLSeQ_H9ZV5_HT71LiGWAQN-iQTdX0814dxFZk1Qdwg7kbNzQWA/viewform" className="help-link"> feedback survey </a> 
+          or send us an <a href="mailto:zestier.than.ever@gmail.com" className="help-link">email</a> with your ideas!</p>
+      </article>
       <img src={RadsImg} alt="radishes" className="rads-img" />
-    </div>
+    </section>
   );
 }
 

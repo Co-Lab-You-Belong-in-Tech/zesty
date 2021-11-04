23e37e7b769e1e52559b2d41955709ad418a2df5
Rebekah <rebekahjanedejong@gmail.com>
2021-11-04 15:24:53 -0400

    Fix Preferences options

--- src/Components/PreferenceOption/PreferenceOption.js
+++ src/Components/PreferenceOption/PreferenceOption.js
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+
+import { makeStyles } from "@mui/styles";
+
 import Accordion from '@mui/material/Accordion';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import AccordionSummary from '@mui/material/AccordionSummary';
@@ -6,19 +9,30 @@ import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import RadioOption from '../RadioOption/RadioOption';
 
-function PreferenceOption({ panelExpanded, title, ariaControls, id, options, onChange, optionState, name, accordionClass, accordionSummaryClass, accordionDetailsClass }) {
+function PreferenceOption({ panelExpanded, title, ariaControls, id, options, onChange, optionState, name }) {
     const [expanded, setExpanded] = useState(false);
 
     const handleChange = (panel) => (event, isExpanded) => {
       setExpanded(isExpanded ? panel : false);
     };
+    
+      const useStyles = makeStyles(() => ({
+        accordion: {
+          marginBottom: 0,
+        },
+        accordionDetails: {
+          backgroundColor: 'white'
+        }
+      }));
+    
+      const classes = useStyles();
 
     return (
         <Accordion
             TransitionProps={{ unmountOnExit: true }}
             expanded={expanded === panelExpanded} 
             onChange={handleChange(panelExpanded)}
-            className={accordionClass}
+            className={classes.accordion}
         >
             <AccordionSummary
                 expandIcon={<ExpandMoreIcon />}
@@ -26,14 +40,14 @@ function PreferenceOption({ panelExpanded, title, ariaControls, id, options, onC
                 id={id}
                 sx={{ bgcolor: 'white' }}
             >
-                <Typography sx={{ fontFamily: 'Montserrat', fontWeight: 500, textAlign: 'left', fontSize: '1.125rem' }}>
+                <Typography sx={{ fontFamily: 'Montserrat', fontWeight: 500, textAlign: 'left', fontSize: '1.125rem', color: 'text.secondary' }}>
                     {title}
                 </Typography>
                 <Typography sx={{ marginLeft: 5, color: 'text.secondary', fontFamily: 'Montserrat', fontSize: '1rem' }}>
                     {optionState}
                 </Typography>
             </AccordionSummary>
-            <AccordionDetails className={accordionDetailsClass}>
+            <AccordionDetails className={classes.accordionDetails}>
                 {options.map((option) => {
                     return (
                         <RadioOption 
